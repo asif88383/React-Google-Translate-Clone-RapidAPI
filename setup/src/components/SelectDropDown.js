cdf6035cbdcbd607c66ff693b1c9e66a35793168
Muhammad Asif <asif88383@gmail.com>
2022-04-15 12:07:06 +0500

    first commit

--- setup/src/components/SelectDropDown.js
+++ setup/src/components/SelectDropDown.js
@@ -1,7 +1,16 @@
 const SelectDropDown = () => {
     return (
-        <div>
-            SelectDropDown
+        <div className="select-drop-down">
+            <input />
+            <div className="down-arrow">
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg"
+                    focusable="false"
+                    viewBox="M7 1015 5 5-5z"
+                >
+                    <path d="M7 1015 5 5-5z" />
+                </svg>
+            </div>
         </div>
     );
 }
--- setup/src/components/TextBox.js
+++ setup/src/components/TextBox.js
@@ -1,6 +1,9 @@
+import SelectDropDown from "./SelectDropDown";
+
 const TextBox = ({style}) => {
     return (
-        <div>
+        <div className={style}>
+            <SelectDropDown />
             <textarea 
                 placeholder={style == 'input' ? 'Enter Text' : 'Translated Text'}
                 disabled={style == 'output'}
